05480a19c99c99730cd5131ff536e865cd864eac
Quentin Rouger <144003341+Qrouger@users.noreply.github.com>
2026-07-13 13:40:46 +0200

    Prevent duplicate PPI entries in all_lines
    Added a check to prevent duplicate entries in done_ppi before appending to all_lines.

--- iQ-score.py
+++ iQ-score.py
@@ -113,8 +113,9 @@ def Score_interaction (msa_dir,model_dir,Path_ccp4,N_CPU) :
                     done_ppi.append(row["jobs"])
                     
             for ppi in ppi_list :
-                line = f"{ppi.split('/')[-1]}_ranked_0,0,0,0,0\n"
-                all_lines = all_lines + line
+                if ppi.split("/")[-1]+"_ranked_0" not in done_ppi :
+                    line = f"{ppi.split('/')[-1]}_ranked_0,0,0,0,0\n"
+                    all_lines = all_lines + line
    
     if len(all_lines.strip("\n")) > 1 : #if all_lines is not empty
             with open(f"./iQ-score_results.csv", "w") as file2 :
